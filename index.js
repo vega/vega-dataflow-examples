6f99bf8a44dbd6fadc20437c35ffd1ab6f16333d
Jeffrey Heer <jeffrey.heer@gmail.com>
2016-08-10 16:02:51 -0700

    Initial commit.

---
+++ index.js
@@ -0,0 +1,10 @@
+export * from 'vega-statistics';
+export * from 'vega-util';
+export * from 'vega-dataflow';
+
+import * as vc from 'vega-crossfilter';
+import * as ve from 'vega-encode';
+import * as vf from 'vega-force';
+import * as vg from 'vega-geo';
+import * as vh from 'vega-hierarchy';
+import * as vv from 'vega-voronoi';
